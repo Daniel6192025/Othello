ce701f8cb247d37cac76cd5c6603cb8a4d71ab06
Daniel Nguyen <daniel.nguyen@mrjaffesclass.com>
2023-11-03 12:46:13 -0700

    added teamName

---
+++ main/java/com/mrjaffesclass/othello/DanielNguyenKayleeNguyenVernonPitney.java
@@ -0,0 +1,14 @@
+package main.java.com.mrjaffesclass.othello;
+
+
+/**
+ * Write a description of class DanielNguyenKayleeNguyenVernonPitney here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DanielNguyenKayleeNguyenVernonPitney extends Player
+{
+    // instance variables - replace the example below with your own
+    
+}
